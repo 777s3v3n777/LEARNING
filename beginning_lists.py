1f8d3a896521cd26fa09fb000b1727ced7cb7ac0
777s3v3n777 <7s3v3n7@protonmail.com>
2016-08-12 20:44:46 -0600

    Create beginning_lists.py

---
+++ beginning_lists.py
@@ -0,0 +1,53 @@
+####name
+#name = "fook yeww"
+#print(name.title())
+#print(name.lower())
+#print(name.upper())
+
+###name_combined_string
+#first_name = "fook"
+#last_name = "yeww"
+#full_name = first_name + " " + last_name
+#print(full_name)
+
+
+####biginner LIST's
+#S3V3N_Greater_than = ['chernobyl', 'havok', 'Greysilverbackbeetch', 'yolo']
+#print(S3V3N_Greater_than[0].upper())
+#print(S3V3N_Greater_than[2].upper())
+
+
+##List's THE MORE COMPLICATED
+#S3V3N_Greater_than = ['chernobyl', 'havok', 'Greysilverbackbeetch', 'yolo']
+#message = "I'm better than everyone, I EZ BEAST :P " + S3V3N_Greater_than[0].upper() + ", " + S3V3N_Greater_than[1].upper() + ", " + S3V3N_Greater_than[2].upper() + "." + " "
+#print(message)
+
+#######Listing options
+#exercise_ways_to_write_name = "Languages:\nfuck\nyou\n!!!!!!!\n S3V3N - yours truly\n;)"
+#print(exercise_ways_to_write_name.title())
+
+###way's to write mame
+#exercise_ways_to_write_name = "Languages:\nfuck\nyou\n!!!!!!!\n S3V3N - yours truly\n;)"
+#print(exercise_ways_to_write_name.title())
+#print(	)
+#print(	)
+#exercise_ways_to_write_name = "select your optoin:\n \t1 fuck\n\t2 you!!!!!!!\n\t3 S3V3N - yours truly\n\t;)"
+#print(exercise_ways_to_write_name.title())
+#print(	)
+#print(	)
+
+
+####BROKENCODE WITH CORRECT CODE
+##BROKEN CODE
+#age = 23
+#message = "Happy " + age + "Birthday"
+#print(message)
+##this isn't going to work because python doesn't know whether or not
+##23 is two characters or the number, so we can actually fix this by 
+##specifying the variable "age" wrapped in a function. str(age) observe!
+
+
+#CORRECT CODE
+age = 1000
+message = "I AM " + str(age) + "YEARS OF AGE - ODIN SLEEP"
+print(message)
